32d841c28733fbb61914e99ec27c790f08dd2171
Chris Strom <eee.c@eeecooks.com>
2011-05-01 15:52:17 -0400

    Compass config

---
+++ config.rb
@@ -0,0 +1,10 @@
+# Require any additional compass plugins here.
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "_site/stylesheets"
+sass_dir = "sass"
+images_dir = "_site/images"
+javascripts_dir = "_site/javascripts"
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+preferred_syntax = :sass
