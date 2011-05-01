067edb33f7614b69427a550f97cd33a99c0289e2
Chris Strom <eee.c@eeecooks.com>
2011-05-01 15:59:07 -0400

    Under normal circumstance, compass needs to generate into root_dir
    Not _site, which is jekyll generated.  I'll need a stash to be able to watch compass changes

--- config.rb
+++ config.rb
@@ -1,10 +1,10 @@
 # Require any additional compass plugins here.
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "_site/stylesheets"
+css_dir = "stylesheets"
 sass_dir = "sass"
-images_dir = "_site/images"
-javascripts_dir = "_site/javascripts"
+images_dir = "images"
+javascripts_dir = "javascripts"
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 preferred_syntax = :sass
