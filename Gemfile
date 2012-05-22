a79770b07700ef29f930e88c300eb0fae2760dfd
Chris Strom <eee.c@eeecooks.com>
2012-05-21 23:38:00 -0400

    Add /real and /push test URLs

--- Gemfile
+++ Gemfile
@@ -2,5 +2,5 @@ source :rubygems
 
 gem "jekyll"
 gem "rdiscount"
-gem "RedCloth"
+gem "RedCloth", "4.2.8"
 gem "compass"
--- app.js
+++ app.js
@@ -37,6 +37,7 @@ app.configure(function(){
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(__dirname + '/_site'));
+  app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
@@ -52,6 +53,39 @@ app.configure('production', function(){
 // app.get('/', routes.index);
 // app.get('/cool', routes.cool);
 // app.post('/cool', routes.cool);
+
+app.get('/real', function(req, res){
+  res.render('real', {
+    title: 'Hello (real) World!'
+  });
+});
+
+
+app.get('/push', function(req, res){
+  function push(relative_path) {
+    res.push(relative_path, {}, function(err, stream) {
+        fs.createReadStream('public/' + relative_path).pipe(stream);
+    });
+  }
+
+  push("/images/00-h.jpg");
+  push("/images/01-e.jpg");
+  push("/images/02-l.jpg");
+  push("/images/03-l.jpg");
+  push("/images/04-o.jpg");
+  push("/images/05-space.jpg");
+  push("/images/06-w.jpg");
+  push("/images/07-o.jpg");
+  push("/images/08-r.jpg");
+  push("/images/09-l.jpg");
+  push("/images/10-d.jpg");
+  push("/images/11-bang.jpg");
+
+  res.render('real', {
+    title: 'Hello (real) World!'
+  });
+});
+
 
 app.listen(4000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
